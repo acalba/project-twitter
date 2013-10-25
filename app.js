98d25c7e7348bf46604fcd822c61cd8f2cc41c43
Aileen Alba <acalba@csupomona.edu>
2013-10-24 19:34:06 -0700

    add scores page

--- app.js
+++ app.js
@@ -44,7 +44,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/scores', function (req, res) {
-    res.render('scores');
+  var mysql      = require('mysql');
+
+  var connection = mysql.createConnection({
+    host     : '10.101.100.95',
+    user     : 'hackathon_user',
+    password : 'onescreen',
+  });
+
+  connection.connect();
+
+  var query = connection.query('SELECT name, score FROM hackathon.scores ORDER BY score DESC LIMIT 10;', function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+  connection.end();
+
+  res.render('scores', { scores : query.values});
 });
 
 app.use(express.bodyParser());
