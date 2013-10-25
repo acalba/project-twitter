f5b8b127f803ad7a12ae76ee2cd869cb77d3f655
Jonathan Yee <raptrex@gmail.com>
2013-10-24 17:13:02 -0700

    added countdown timer

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,3 +1,30 @@
 $(function() {
-  console.log('ready');
+  var $countdown = $("#countdown h2");
+  var $game = $("#game");
+  var $tweetList = $game.find("ul");
+
+  function startRound(roundNumber) {
+    var time = 1;
+    $game.hide();
+    $countdown.text(time);
+    
+    var timer = self.setInterval(function() {
+      if (time > 0) {
+        time--;
+        $countdown.text(time);    
+      } else {
+        clearInterval(timer);
+        $countdown.hide();
+        $game.show();
+        highlightLine(1);
+      }
+    }, 1000);
+  }
+
+  function highlightLine(index) {
+    console.log($tweetList);
+  }
+
+  startRound();
+  
 });
