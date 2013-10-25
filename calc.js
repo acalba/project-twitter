f094ceb185fd8a37a39b05a726133a7d616c2506
richard2191 <richard2191@yahoo.com>
2013-10-24 23:37:10 -0700

    deleted unused files

--- calc.js
+++
@@ -1,6 +0,0 @@
-function calculateScore(length, speed, count)
-{
-	var score = (length / 140 * 250) + (2 * speed) - (0.5 * count);
-	alert("Your score is " + Math.ceil(score) + "!\nTweet Length: " + length + " characters\nYour typing speed: " + speed + " wpm\nWrong Characters Typed: " + count + " characters");
-	return score;
-}
